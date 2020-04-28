de5a7865b335a551e7d4096b23fcd0066c501c1a
Ale Mohamad <hello@alemohamad.com>
2020-04-28 23:24:19 +0200

    App Design and Layout - 1 - Section 2

--- Landmarks/Landmarks/Home.swift
+++ Landmarks/Landmarks/Home.swift
@@ -9,10 +9,21 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
+    
     var body: some View {
         NavigationView {
-            Text("Landmarks Content")
-                .navigationBarTitle(Text("Featured"))
+            List {
+                ForEach(categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationBarTitle(Text("Featured"))
         }
     }
 }
